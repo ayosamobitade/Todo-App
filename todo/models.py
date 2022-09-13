49cedd6d0c919064863d5d96a1952bef8dc77b04
UncleAyo <ayosamobitade@gmail.com>
2022-09-13 22:33:53 +0100

    some updates

--- todo/models.py
+++ todo/models.py
@@ -6,8 +6,12 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     title = models.CharField(max_length = 100)
     memo = models.TextField(blank = True)
+
+    # set to the current time
     created = models.DateTimeField(auto_now_add = True)
     completed = models.BooleanField(default = False)
+
+    # user who posted this
     user = models.ForeignKey(User, on_delete = models.CASCADE)
     slug = models.SlugField(unique = True)
 
