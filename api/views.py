5ab82fdee9994e4874c020377581168c8c495183
UncleAyo <ayosamobitade@gmail.com>
2022-09-13 22:29:30 +0100

    some updates

--- api/views.py
+++ api/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from rest_framework.generics import ListCreateAPIView
-from .serializers import TodoSerializer
+from .serializers import TodoSerializer, TodocompleteSerializer
 from todo.models import Todo
 from rest_framework import permissions
+
 
 
 class  TodoList(ListCreateAPIView):
@@ -19,10 +20,11 @@ class  TodoList(ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user = self.request.user)
 
-class TodoRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIViews):
+class TodoRetrieveUpdateDestroy(RetrieveUpdateDestroyAPIView):
     serializer_class = TodoSerializer
     permissions = [permissions.IsAuthenticated]
 
     def get_query(self):
         user = self.request.user
         return Todo.objects.filter(user = user)
+
